2f2dc6eef6af47697feb1089a77fd9bec41c1f7c
Seong-Kyu Yun <reviday@naver.com>
2020-01-07 14:46:14 +0900

    #11. Connecting the Components to State

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Timer from './components/Timer';
+import reducer from './reducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-export default function App() {
-  return (
-    <Timer>test</Timer>
-  );
+let store = createStore(reducer);
+
+console.log(store.getState());
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Timer/>
+      </Provider>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
